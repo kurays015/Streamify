5f0f42aa19899843c230c81f9be4723cf6ab6666
Christzxz <christnarvarte15@gmail.com>
2024-01-08 21:27:25 +0800

    change login button to color white

--- app/page.jsx
+++ app/page.jsx
@@ -2,9 +2,11 @@ import Link from "next/link";
 
 export default async function Home() {
   return (
-    <main className="">
+    <main className="text-center">
       <h1 className="text-white text-xl">home page!</h1>
-      <Link href="/signin">Login</Link>
+      <Link href="/signin" className="text-white">
+        Login
+      </Link>
     </main>
   );
 }
