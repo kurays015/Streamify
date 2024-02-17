27a21bd44b09532850369c97fedc979adecd96a7
Christzxz <christnarvarte15@gmail.com>
2024-02-17 16:33:46 +0800

    add key prop to EpisodeNumber comp

--- components/episodes/FilteredEpisodes.jsx
+++ components/episodes/FilteredEpisodes.jsx
@@ -6,7 +6,7 @@ export default function FilteredEpisodes({ filteredEpisodes, info }) {
   return (
     <div className="grid grid-cols-5 gap-2 my-5 max-h-96 overflow-auto scrollbar-gray customSm:grid-cols-5 customSemiMd:grid-cols-6 customSemiMd2:grid-cols-7 md:grid-cols-8 lg:grid-cols-9 xl:grid-cols-12">
       {filteredEpisodes.map(episode => (
-        <EpisodeNumber {...episode} info={info} />
+        <EpisodeNumber key={episode.id} {...episode} info={info} />
       ))}
     </div>
   );
