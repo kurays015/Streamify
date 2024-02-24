eda3790504514407205abe0c321818fc4d01c679
Christzxz <christnarvarte15@gmail.com>
2024-02-24 22:25:34 +0800

    add Link to home in logo

--- components/Header.jsx
+++ components/Header.jsx
@@ -6,10 +6,10 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header className="flex items-center justify-between text-white py-8 px-2 max-w-7xl mx-auto z-50 relative">
-      <div className="flex items-center gap-3">
+      <Link href="/" className="flex items-center gap-3">
         <LuPopcorn className="text-4xl text-red-500" />
         <h1 className="uppercase font-bold text-xl text-teal-500">Streamify</h1>
-      </div>
+      </Link>
       <nav>
         <ul className="flex items-center gap-5">
           <li>
