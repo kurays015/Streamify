8de9e8077145c5b7fc612fc9ec8a38e17b4ec048
Christzxz <christnarvarte15@gmail.com>
2024-02-25 23:50:09 +0800

    revert middleware to old state

--- middleware.js
+++ middleware.js
@@ -3,15 +3,18 @@ import { NextResponse } from "next/server";
 
 export default async function middleware(req) {
   const token = await getToken({ req });
-  // const protectedRoutes =
-  //   req.nextUrl.pathname.startsWith("/movie-series") ||
-  //   req.nextUrl.pathname.startsWith("/info") ||
-  //   req.nextUrl.pathname.startsWith("/watch");
+  const protectedRoutes =
+    req.nextUrl.pathname.startsWith("/movie-series") ||
+    req.nextUrl.pathname.startsWith("/info") ||
+    req.nextUrl.pathname.startsWith("/watch");
 
   const signInRoute = req.nextUrl.pathname.startsWith("/signin");
 
-  if (!token) {
+  if (!token && protectedRoutes) {
     return NextResponse.redirect(new URL("/signin", req.url));
-  } else if (token && signInRoute)
+  }
+
+  if (token && signInRoute) {
     return NextResponse.redirect(new URL("/", req.url));
+  }
 }
