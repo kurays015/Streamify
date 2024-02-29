53893d5eb3a90b6234cf63ced82b65db2822754d
Christzxz <christnarvarte15@gmail.com>
2024-02-29 21:42:24 +0800

    change video source

--- app/(watch)/embedded/page.jsx
+++ app/(watch)/embedded/page.jsx
@@ -31,14 +31,16 @@ export default function EmbeddedVideoPage() {
 
   return (
     <main
-      className={`relative ${
-        type === "TV Series" ? "max-w-7xl mx-auto h-screen lg:mt-12" : ""
+      className={`relative  ${
+        type === "TV Series" ? "max-w-7xl mx-auto lg:mt-12 " : ""
       }`}
     >
       {searchParams.toString() && <EmbeddedVideo type={type} params={params} />}
-      <h1 className="text-slate-300 font-semibold text-center mt-5 customSm:p-2 customSm:text-base md:text-lg lg:text-xl lg:p-0">
-        {title}
-      </h1>
+      {type === "TV Series" && (
+        <h1 className="text-slate-300 font-semibold text-center mt-5 customSm:p-2 customSm:text-base md:text-lg lg:text-xl lg:p-0">
+          {title}
+        </h1>
+      )}
       {type === "TV Series" && (
         <div className="customSm:p-2 lg:p-0">
           <ReusableSelect info={info} />
--- components/embedded/EmbeddedVideo.jsx
+++ components/embedded/EmbeddedVideo.jsx
@@ -3,10 +3,11 @@ export default function EmbeddedVideo({ type, params }) {
     <iframe
       className={`${
         type === "Movie"
-          ? "h-screen w-screen"
+          ? "h-screen w-full"
           : "customSm:h-[40vh] md:h-[60vh] lg:h-[70vh] w-full"
       } rounded-md`}
-      src={`https://vidsrc.xyz/embed/${params}`}
+      src={`https://vidsrc.to/embed/${params}`}
+      allowFullScreen
     ></iframe>
   );
 }
