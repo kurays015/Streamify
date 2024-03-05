6722e1dbdd832ffa1992281d85aa8edbe6b5f5db
Christzxz <christnarvarte15@gmail.com>
2024-03-05 14:04:51 +0800

    comment code in read page, add holy loader

--- app/(read-manga)/read/[id]/page.jsx
+++ app/(read-manga)/read/[id]/page.jsx
@@ -1,64 +1,39 @@
-import ChaptersButton from "@/components/ChaptersButton";
-import ChaptersContent from "@/components/ChaptersContent";
-import NoContent from "@/components/NoContent";
-import WatchAndInfoError from "@/components/WatchAndInfoError";
+import { MANGA } from "@consumet/extensions";
 
-async function getChaptersPages(id, searchParams, reverseChapters) {
-  const { chapter, readId } = searchParams;
-  const firstChapterReadId = reverseChapters[0].id;
-  try {
-    const res = await fetch(
-      `${process.env.ANIFY_URL}/pages/${id}/${chapter ? chapter : 1}/comick/${
-        readId ? readId : firstChapterReadId
-      }`
-    );
-    if (!res.ok) {
-      throw new Error("Error fetching chapter pages.");
-    }
-    return res.json();
-  } catch (error) {
-    console.log(error);
-  }
-}
+// async function getChaptersPages(id, searchParams, reverseChapters) {
+//   const { chapter, readId } = searchParams;
+//   const firstChapterReadId = reverseChapters[0].id;
+//   try {
+//     const res = await fetch(
+//       `${process.env.ANIFY_URL}/pages/${id}/${chapter ? chapter : 1}/comick/${
+//         readId ? readId : firstChapterReadId
+//       }`
+//     );
+//     if (!res.ok) {
+//       throw new Error("Error fetching chapter pages.");
+//     }
+//     return res.json();
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
 
-async function getChapters(id) {
-  try {
-    const res = await fetch(`${process.env.ANIFY_URL}/chapters/${id}`);
-    if (!res.ok) {
-      throw new Error("Error fetching chapters.");
-    }
-    return res.json();
-  } catch (error) {
-    console.log(error);
-  }
-}
+// async function getChapters(id) {
+//   try {
+//     const res = await fetch(`${process.env.ANIFY_URL}/chapters/${id}`);
+//     if (!res.ok) {
+//       throw new Error("Error fetching chapters.");
+//     }
+//     return res.json();
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
 
-export default async function MangaMainContent({ params, searchParams }) {
-  // const providers = await getChapters(params.id);
-  // const comickChapters = providers?.find(
-  //   provider => provider.providerId === "comick"
-  // );
-  // const reverseChapters = comickChapters.chapters.sort(
-  //   (a, b) => a.number - b.number
-  // );
-  // const chaptersPages = await getChaptersPages(
-  //   params.id,
-  //   searchParams,
-  //   reverseChapters
-  // );
-
-  // if (!chaptersPages) return <WatchAndInfoError />;
-
+export default async function MangaMainContent() {
   return (
     <div className="text-white">
       <h1>in development...</h1>
-      {/* <ChaptersButton reverseChapters={reverseChapters} />
-      {chaptersPages ? (
-        <ChaptersContent chaptersContent={chaptersPages} />
-      ) : (
-        <NoContent />
-      )}
-      <ChaptersButton reverseChapters={reverseChapters} /> */}
     </div>
   );
 }
--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import MetaContextProvider from "@/context/MetaContext";
+import HolyLoader from "holy-loader";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,13 @@ export default async function RootLayout({ children }) {
         suppressHydrationWarning
       >
         <MetaContextProvider>
+          <HolyLoader
+            color="#ff4500"
+            height=".2rem"
+            speed={250}
+            easing="linear"
+            showSpinner
+          />
           <Header />
           {children}
         </MetaContextProvider>
