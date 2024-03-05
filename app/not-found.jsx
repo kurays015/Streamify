58daf15d0fbf137bbc629d1042236147a2fbe64c
Christzxz <christnarvarte15@gmail.com>
2024-03-06 00:40:05 +0800

    add not found in search and make custom not-found.jsx

---
+++ app/not-found.jsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import notfound from "@/public/404.svg";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <main className="relative text-center">
+      <div className="flex justify-center">
+        <Image src={notfound} alt="not-found" priority />
+      </div>
+      <p className="my-2 text-slate-300">You&apos;re lost!</p>
+      <Link href="/">
+        <Button variant="secondary">Go back home</Button>
+      </Link>
+    </main>
+  );
+}
