228a2f3605b8acb0b9f501f39a9da8eecc6eb607
Christzxz <christnarvarte15@gmail.com>
2024-03-13 01:42:55 +0800

    add kofi in footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,3 +1,7 @@
+import kofi from "@/public/kofi.webp";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Footer() {
   return (
     <footer className="max-w-7xl mx-auto text-slate-400 customSm:mt-16 customSm:mb-5 customSm:p-3 customSm:text-center lg:text-start lg:mt-32 lg:mb-8 lg:py-2">
@@ -9,6 +13,12 @@ export default function Footer() {
         provided by non-affiliated third parties.
       </small>
       <p className="text-xs">Made with 🖤 by Christ</p>
+      <div className="flex items-center gap-2 text-xs my-1 customSm:justify-center lg:justify-start">
+        Support me{" "}
+        <Link href="https://ko-fi.com/christt" target="_blank">
+          <Image src={kofi} alt="kofi-link" width={50} height={50} />
+        </Link>
+      </div>
     </footer>
   );
 }
