4a4576ac40649edd41e06ae5457c9f380c3ebf93
Christzxz <christnarvarte15@gmail.com>
2024-04-01 20:59:50 +0800

    add opt chaining

--- components/infos/Title.jsx
+++ components/infos/Title.jsx
@@ -4,5 +4,5 @@ import titleHandler from "@/lib/titleHandler";
 
 export default function Title({ info }) {
   console.log(info, "TITLE !");
-  return <div>{titleHandler(info.title)}</div>;
+  return <div className="text-white">{titleHandler(info?.title)}</div>;
 }
