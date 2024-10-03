7180b48b028dc5b836c7cfa36d4af6d9487aaed8
Christzxz <126612073+kurays015@users.noreply.github.com>
2024-10-04 03:36:28 +0800

    uncomment middleware

--- middleware.js
+++ middleware.js
@@ -19,22 +19,22 @@ export default auth(req => {
   const isPublicRoute = publicRoutes.includes(pathname);
   const isAuthRoute = authRoutes.includes(pathname);
 
-  // if (isApiAuthRoute) {
-  //   return null;
-  // }
+  if (isApiAuthRoute) {
+    return null;
+  }
 
-  // if (isAuthRoute) {
-  //   if (isAuthenticated) {
-  //     return NextResponse.redirect(
-  //       new URL(DEFAULT_LOGIN_REDIRECT, req.nextUrl)
-  //     );
-  //   }
-  //   return null;
-  // }
+  if (isAuthRoute) {
+    if (isAuthenticated) {
+      return NextResponse.redirect(
+        new URL(DEFAULT_LOGIN_REDIRECT, req.nextUrl)
+      );
+    }
+    return null;
+  }
 
-  // if (!isAuthenticated && !isPublicRoute) {
-  //   return NextResponse.redirect(new URL("/signin", req.nextUrl));
-  // }
+  if (!isAuthenticated && !isPublicRoute) {
+    return NextResponse.redirect(new URL("/signin", req.nextUrl));
+  }
 
   return null;
 });
