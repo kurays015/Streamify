2737ed2834fd08f1a45d875106fdf680c37f7642
Christzxz <christnarvarte15@gmail.com>
2024-10-05 13:53:23 +0800

    comment5

--- components/infos/MetaInfo.jsx
+++ components/infos/MetaInfo.jsx
@@ -2,27 +2,27 @@ import { providerUrl } from "@/lib/infoUrl";
 import MainInfoContent from "./MainInfoContent";
 import WatchAndInfoError from "../WatchAndInfoError";
 
-export async function getMetaInfo(id, searchParams) {
-  const { providerId, tmdbParams } = searchParams;
-  const tmdb = `?type=${tmdbParams}`;
-  const anilistManga = "?provider=mangadex";
-  const params = providerId === "tmdb" ? tmdb : anilistManga;
+// export async function getMetaInfo(id, searchParams) {
+//   const { providerId, tmdbParams } = searchParams;
+//   const tmdb = `?type=${tmdbParams}`;
+//   const anilistManga = "?provider=mangadex";
+//   const params = providerId === "tmdb" ? tmdb : anilistManga;
 
-  try {
-    const res = await fetch(
-      `${process.env.SOURCE_URL8}${providerUrl(providerId, id, params)}`,
-      {
-        cache: "no-store",
-      }
-    );
-    if (!res.ok) {
-      throw new Error("Error fetching anime/manga/movie/series info.");
-    }
-    return res.json();
-  } catch (error) {
-    console.log(error);
-  }
-}
+//   try {
+//     const res = await fetch(
+//       `${process.env.SOURCE_URL8}${providerUrl(providerId, id, params)}`,
+//       {
+//         cache: "no-store",
+//       }
+//     );
+//     if (!res.ok) {
+//       throw new Error("Error fetching anime/manga/movie/series info.");
+//     }
+//     return res.json();
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
 
 export default async function MetaInfo({ id, searchParams }) {
   // const info = await getMetaInfo(id, searchParams);
@@ -30,5 +30,5 @@ export default async function MetaInfo({ id, searchParams }) {
   // if (!info) return <WatchAndInfoError />;
 
   // return <MainInfoContent infoData={info} id={id} />;
-  return <h1>Maintenance?!...</h1>;
+  return <h1 className="text-white">Maintenance?!...</h1>;
 }
