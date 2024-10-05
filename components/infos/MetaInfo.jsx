5319c1fc7c5b617ae2aeafd084ae410f5e6c2dfc
Christzxz <christnarvarte15@gmail.com>
2024-10-05 14:56:19 +0800

    render title, test3

--- components/infos/MetaInfo.jsx
+++ components/infos/MetaInfo.jsx
@@ -2,31 +2,34 @@ import { providerUrl } from "@/lib/infoUrl";
 import MainInfoContent from "./MainInfoContent";
 import WatchAndInfoError from "../WatchAndInfoError";
 
-export async function getMetaInfo(id, searchParams) {
-  const { providerId, tmdbParams } = searchParams;
-  const tmdb = `?type=${tmdbParams}`;
-  const anilistManga = "?provider=mangadex";
-  const params = providerId === "tmdb" ? tmdb : anilistManga;
+// export async function getMetaInfo(id, searchParams) {
+//   const { providerId, tmdbParams } = searchParams;
+//   const tmdb = `?type=${tmdbParams}`;
+//   const anilistManga = "?provider=mangadex";
+//   const params = providerId === "tmdb" ? tmdb : anilistManga;
 
-  try {
-    const res = await fetch(
-      `https://consumet-api-o7fr.onrender.com/meta/tmdb/info/519182?type=movie`,
-      // `${process.env.SOURCE_URL8}${providerUrl(providerId, id, params)}`,
-      {
-        cache: "no-store",
-      }
-    );
-    if (!res.ok) {
-      throw new Error("Error fetching anime/manga/movie/series info.");
-    }
-    return res.json();
-  } catch (error) {
-    console.log(error);
-  }
-}
+//   try {
+//     const res = await fetch(
+//       `${process.env.SOURCE_URL8}${providerUrl(providerId, id, params)}`,
+//       {
+//         cache: "no-store",
+//       }
+//     );
+//     if (!res.ok) {
+//       throw new Error("Error fetching anime/manga/movie/series info.");
+//     }
+//     return res.json();
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
 
 export default async function MetaInfo({ id, searchParams }) {
-  const info = await getMetaInfo(id, searchParams);
+  // const info = await getMetaInfo(id, searchParams);
+  const res = await fetch(
+    "https://consumet-api-o7fr.onrender.com/meta/tmdb/info/519182?type=movie"
+  );
+  const info = await res.json();
 
   if (!info) return <WatchAndInfoError />;
 
