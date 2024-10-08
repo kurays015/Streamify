192a8c6cb5e3de4215f1eedcbc3b7ccd2d454cee
Christzxz <christnarvarte15@gmail.com>
2024-10-09 00:18:01 +0800

    add force dynamic again

--- app/(all-info)/info/[id]/page.jsx
+++ app/(all-info)/info/[id]/page.jsx
@@ -11,7 +11,7 @@
 //   return infoMetadata(metaInfo, previousImages);
 // };
 
-// export const dynamic = "force-dynamic";
+export const dynamic = "force-dynamic";
 
 export default async function AllMetaInfo({ params, searchParams }) {
   const res = await fetch(
