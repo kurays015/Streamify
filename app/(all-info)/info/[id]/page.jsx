32fc9a36bb87ebcb01b2f2cb51b00ec31c8a08eb
Christzxz <christnarvarte15@gmail.com>
2024-10-08 23:28:10 +0800

    hardcode api url

--- app/(all-info)/info/[id]/page.jsx
+++ app/(all-info)/info/[id]/page.jsx
@@ -1,23 +1,33 @@
 import MetaInfo, { getMetaInfo } from "@/components/infos/MetaInfo";
 import PikachuLoading from "@/components/skeletons/PikachuLoading";
-// import infoMetadata from "@/lib/infoMetadata";
+import infoMetadata from "@/lib/infoMetadata";
 import { Suspense } from "react";
 
-// export const generateMetadata = async ({ params, searchParams }, parent) => {
-//   const metaInfo = await getMetaInfo(params.id, searchParams);
+export const generateMetadata = async ({ params, searchParams }, parent) => {
+  const metaInfo = await getMetaInfo(params.id, searchParams);
 
-//   const previousImages = (await parent).openGraph?.images || [];
+  const previousImages = (await parent).openGraph?.images || [];
 
-//   return infoMetadata(metaInfo, previousImages);
-// };
+  return infoMetadata(metaInfo, previousImages);
+};
 
 export default async function AllMetaInfo({ params, searchParams }) {
+  const res = await fetch(
+    "https://consumet-api-o7fr.onrender.com/meta/tmdb/info/519182?type=movie"
+  );
+  const info = await res.json();
+
+  if (!info) return <div className="text-white text-xl">Nothing...</div>;
+
   return (
     <main>
-      <h1 className="text-white">Info Page...</h1>
-      <Suspense fallback={<PikachuLoading />}>
+      <h1 className="text-white">{info.title ? info.title : "no response "}</h1>
+      <h1 className="text-white">
+        {info.description ? info.description : "no response "}
+      </h1>
+      {/* <Suspense fallback={<PikachuLoading />}>
         <MetaInfo id={params.id} searchParams={searchParams} />
-      </Suspense>
+      </Suspense> */}
     </main>
   );
 }
--- components/infos/MetaInfo.jsx
+++ components/infos/MetaInfo.jsx
@@ -2,45 +2,32 @@ import { providerUrl } from "@/lib/infoUrl";
 import MainInfoContent from "./MainInfoContent";
 import WatchAndInfoError from "../WatchAndInfoError";
 
-// export async function getMetaInfo(id, searchParams) {
-//   const { providerId, tmdbParams } = searchParams;
-//   const tmdb = `?type=${tmdbParams}`;
-//   const anilistManga = "?provider=mangadex";
-//   const params = providerId === "tmdb" ? tmdb : anilistManga;
+export async function getMetaInfo(id, searchParams) {
+  const { providerId, tmdbParams } = searchParams;
+  const tmdb = `?type=${tmdbParams}`;
+  const anilistManga = "?provider=mangadex";
+  const params = providerId === "tmdb" ? tmdb : anilistManga;
 
-//   try {
-//     const res = await fetch(
-//       `${process.env.SOURCE_URL8}${providerUrl(providerId, id, params)}`,
-//       {
-//         cache: "no-store",
-//       }
-//     );
-//     if (!res.ok) {
-//       throw new Error("Error fetching anime/manga/movie/series info.");
-//     }
-//     return res.json();
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
+  try {
+    const res = await fetch(
+      `${process.env.SOURCE_URL8}${providerUrl(providerId, id, params)}`,
+      {
+        cache: "no-store",
+      }
+    );
+    if (!res.ok) {
+      throw new Error("Error fetching anime/manga/movie/series info.");
+    }
+    return res.json();
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 export default async function MetaInfo({ id, searchParams }) {
-  // const info = await getMetaInfo(id, searchParams);
-  // const res = await fetch(
-  //   "https://consumet-api-o7fr.onrender.com/meta/tmdb/info/519182?type=movie"
-  // );
-  const res = await fetch(
-    "https://consumet-api-o7fr.onrender.com/meta/tmdb/info/519182?type=movie"
-  );
-  const info = await res.json();
+  const info = await getMetaInfo(id, searchParams);
 
   if (!info) return <WatchAndInfoError />;
 
-  return (
-    <h1 className="text-white">
-      {info.title ? info.title : "No api response"}, Test
-    </h1>
-  );
-
-  // return <MainInfoContent infoData={info} id={id} />;
+  return <MainInfoContent infoData={info} id={id} />;
 }
