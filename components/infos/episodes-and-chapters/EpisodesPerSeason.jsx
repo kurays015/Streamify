6bf7a620a9a3d140855b9d658f35748937e3421b
Christzxz <christnarvarte15@gmail.com>
2024-11-14 21:23:32 +0800

    remove unused

--- components/infos/episodes-and-chapters/EpisodesPerSeason.jsx
+++ components/infos/episodes-and-chapters/EpisodesPerSeason.jsx
@@ -8,9 +8,9 @@ import Link from "next/link";
 async function getSeasons(id, searchParams) {
   try {
     const res = await fetch(
-      `https://api.themoviedb.org/3/tv/${id}/season/${
+      `${process.env.TMDB_BASE_URL}/3/tv/${id}/season/${
         searchParams?.season ?? "0"
-      }?api_key=5e8b9e4f3d246ecf770aa76bb470e4c8`
+      }?api_key=${process.env.API_KEY}`
     );
     return res.json();
   } catch (error) {
@@ -33,15 +33,7 @@ export default async function EpisodesPerSeason({ info, searchParams }) {
   return (
     <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 text-white max-h-[700px] overflow-auto scrollbar-gray w-full mt-8">
       {episodesPerSeasonsWithBase64?.map(
-        ({
-          id,
-          season_number,
-          episode_number,
-          still_path,
-          name,
-          img,
-          base64,
-        }) => (
+        ({ id, season_number, episode_number, name, img, base64 }) => (
           <Link
             key={id}
             href={`/embedded/${titleHandler(
