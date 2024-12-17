9bff2836aafef748c84f0c0a95952b34a4d7f726
christ <christnarvarte15@gmail.com>
2024-12-18 03:28:33 +0800

    add favicon, fix signin page

--- components/LoginPage.jsx
+++ components/LoginPage.jsx
@@ -2,7 +2,7 @@ import ProvidersButtons from "./ProvidersButtons";
 
 export default function LoginPage() {
   return (
-    <div className="px-6 py-12 lg:px-8 z-50 absolute top-[40%] left-1/2 -translate-y-1/2 -translate-x-1/2 w-[600px]">
+    <div className="px-6 py-12 lg:px-8 z-50 absolute top-[40%] left-1/2 -translate-y-1/2 -translate-x-1/2 w-[300px] sm:w-[600px]">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
         <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">
           Sign in to your account
--- components/LoginPageCover.jsx
+++ components/LoginPageCover.jsx
@@ -2,7 +2,7 @@ import Overlay from "./Overlay";
 
 export default function LoginPageCover() {
   return (
-    <div className="h-screen bg-login-cover bg-contain bg-center relative">
+    <div className="h-screen bg-login-cover bg-cover bg-center relative">
       <Overlay />
     </div>
   );
